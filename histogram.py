8b3448f652e4e1dcabb61c0574178a24b345bf90
Nil <nminotte@2D1.42angouleme.fr>
2025-05-20 12:15:40 +0200

    all histo

--- histogram.py
+++ histogram.py
@@ -19,34 +19,41 @@ def load(path: str, index_column=None) -> pd.DataFrame:
 
 
 def plot_histograms(data):
-    try:
-        '''Show the histogram by features'''
-        houses = data["Hogwarts House"].unique()
-        features = [col for col in data.columns if col not in ["Index",\
-            "Hogwarts House", "First Name", "Last Name", "Birthday",\
-            "Best Hand"]]
-        house_colors = {
-            "Gryffindor": "#740001",
-            "Hufflepuff": "#EEBA35",
-            "Ravenclaw": "#0F1D4A", 
-            "Slytherin": "#1A472A" 
-        }
-        for feature in features:
-            plt.figure(figsize=(10, 6))
-            for house in houses:
-                house_data = data[data["Hogwarts House"] == house][feature]
-                house_data = house_data.dropna() # remove the NAN value
-                plt.hist(house_data, alpha=0.6, bins=20, label=house, color=house_colors.get(house, 'pink'))
+    '''Show all histograms on a single page using subplots'''
+    houses = data["Hogwarts House"].unique()
+    features = [col for col in data.columns if col not in ["Index", "Hogwarts House", "First Name", "Last Name", "Birthday", "Best Hand"]]
+    house_colors = {
+        "Gryffindor": "#740001",
+        "Hufflepuff": "#EEBA35",
+        "Ravenclaw": "#0F1D4A",
+        "Slytherin": "#1A472A"
+    }
 
-            plt.title(f"Histogram of {feature} by House")
-            plt.xlabel("Score")
-            plt.ylabel("Number of Students")
-            plt.legend()
-            plt.grid(True)
-            plt.show()
+    num_features = len(features)
+    cols = 3
+    rows = (num_features + cols - 1) // cols
 
-    except Exception as e:
-        print(f"Error: {e}")
+    fig, axes = plt.subplots(rows, cols, figsize=(cols * 5, rows * 4))
+    axes = axes.flatten()
+
+    for i, feature in enumerate(features):
+        ax = axes[i]
+        for house in houses:
+            house_data = data[data["Hogwarts House"] == house][feature].dropna()
+            ax.hist(house_data, alpha=0.6, bins=20, label=house, color=house_colors.get(house, 'pink'))
+
+        ax.set_title(feature)
+        ax.set_xlabel("Score")
+        ax.set_ylabel("Count")
+        ax.grid(True)
+        ax.legend()
+
+    # remove the grid if features < rows * cols
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.tight_layout()
+    plt.show()
 
 
 def main():
